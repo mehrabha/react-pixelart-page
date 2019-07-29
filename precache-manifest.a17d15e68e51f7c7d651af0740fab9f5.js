e26f539e808c36ceb93f60828199e12cfaf3f5f8
mehrabha <mehrabhfz@gmail.com>
2019-07-29 13:04:06 -0400

    Updates

---
+++ precache-manifest.a17d15e68e51f7c7d651af0740fab9f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2002a8fa49ef79979fb88d19dcd39cfe",
+    "url": "/react-pixelart-page/index.html"
+  },
+  {
+    "revision": "d9790395253adefb1d9c",
+    "url": "/react-pixelart-page/static/css/main.c110a42e.chunk.css"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "/react-pixelart-page/static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "d9790395253adefb1d9c",
+    "url": "/react-pixelart-page/static/js/main.e7add9ae.chunk.js"
+  },
+  {
+    "revision": "2b3274c646472ef99ab8",
+    "url": "/react-pixelart-page/static/js/runtime~main.9b18d011.js"
+  }
+]);
